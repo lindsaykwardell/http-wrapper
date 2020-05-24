9ac26e9a82dab5a4af40e234047831387b659ce7
Constantin Apetrei <cstapetrei@yahoo.com>
2020-05-24 18:31:50 +0300

    added static handler

---
+++ StaticHandler.ts
@@ -0,0 +1,36 @@
+import { ServerRequest } from "https://deno.land/std/http/server.ts";
+import { parse, join, sep } from "https://deno.land/std/path/mod.ts";
+
+export class StaticHandler {
+    private localFolderPath: string = '';
+    public staticUrlPrefix: string = '';
+
+    constructor(localFolderPath: string, urlPrefix: string = ''){
+        this.localFolderPath = localFolderPath;
+        this.staticUrlPrefix = urlPrefix;
+        if (!urlPrefix){
+            this.staticUrlPrefix = parse(localFolderPath).base;
+        }
+        if (this.staticUrlPrefix[0] !== '/'){
+            this.staticUrlPrefix = `/${this.staticUrlPrefix}`;
+        }
+    }
+
+    public async process(req:ServerRequest){
+        let responseObject:any = { status: 200 };
+        try {
+          let localFile = join(this.localFolderPath, req.url.replace(this.staticUrlPrefix, '').replace('/', sep));
+          let data = await Deno.readFile(localFile);
+          if (!data){
+            throw new Deno.errors.NotFound();
+          }
+          responseObject.body = data;
+        } catch (error) {
+         responseObject.status = 401;
+          if (error.name === 'NotFound'){
+            responseObject.status = 404;
+          }
+        }
+        req.respond(responseObject);
+    }
+}
